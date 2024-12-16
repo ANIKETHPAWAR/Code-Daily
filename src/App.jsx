1dda9a7fbc55cd909da1c41b5bfdc933d991e01a
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2024-12-16 17:53:16 +0530

    streak 3 stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,30 @@
-import { useState } from 'react'
+import { useState, useRef ,} from 'react'
 import { useEffect } from 'react';
 import { BrowserRouter,Routes,Route,Link , Outlet} from 'react-router-dom';
 import './App.css'
-
+//a clock with a start and stop
 function App() {
- 
-    return <div>
-        
- <BrowserRouter>
- <Routes>
-    <Route path='/' element={<Layout/>}>  
-    <Route path='/neet/online-coaching-class-11' element={<Class11Program/>}/>
-    <Route path='/neet/online-coaching-class-12' element={<Class12Program/>}/>
-    
-    </Route>
- </Routes>
- </BrowserRouter>
-
-    </div>
+ const [currentCount,setCurrentCount] = useState(1)
+ const timer = useRef();
 
 
+function startClock(){
+  let value =   setInterval(function(){
+        setCurrentCount(c => c + 1);
+    }, 1000);
+    timer.current = value;
+}
+function StopClock(){
+   clearInterval(timer.current);
+}
 
-}
-function Class11Program(){
-    return <div>
-        Class11 Program For NEET
-    </div>
-}
-function Class12Program(){
-    return <div style={{color:"Black"}}>
-        Class12 Program For NEET
-    </div>
-}
-function Header(){
-    return <div>
-        Welcome to allen 
-        <Link to="/">Home |</Link> 
- <Link to="/neet/online-coaching-class-11">Class 11 |</Link> 
- <Link to="/neet/online-coaching-class-12">Class 12</Link>
-    </div>
-}
-function Layout(){
-    return <div style={{backgroundColor:"red",}}>
-        <Header/>
-        <div style={{backgroundColor:"white" , height:"100vh",width:"100vw"}}>
-        <Outlet/>
-        </div>
-        footer
-    </div>
+ return <div>
+    {currentCount}
+    <br />
+    <button onClick={startClock}>Start</button>
+    <button onClick={StopClock}>Stop</button>
+
+ </div>
 }
 
 
