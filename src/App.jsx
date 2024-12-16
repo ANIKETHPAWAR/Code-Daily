b0044c1387da53eb6ad810ad6e04e28b19d4878a
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2024-12-16 16:44:48 +0530

    Streak 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
 import { useState } from 'react'
 import { useEffect } from 'react';
-
+import { BrowserRouter,Routes,Route,Link , Outlet} from 'react-router-dom';
 import './App.css'
 
 function App() {
-  return  <div>
-<Card>
+ 
+    return <div>
+        
+ <BrowserRouter>
+ <Routes>
+    <Route path='/' element={<Layout/>}>  
+    <Route path='/neet/online-coaching-class-11' element={<Class11Program/>}/>
+    <Route path='/neet/online-coaching-class-12' element={<Class12Program/>}/>
     
-    <img src="https://mcdn.wallpapersafari.com/medium/52/15/yhTOi2.jpg" alt="" />
-</Card> 
-<Card> 
-  <img src="https://mcdn.wallpapersafari.com/medium/15/27/erAWjz.jpg" alt="" />
-     </Card>
-<Card>
-    <img src="https://mcdn.wallpapersafari.com/medium/15/63/EzIXko.jpg" alt="" />
-</Card>
+    </Route>
+ </Routes>
+ </BrowserRouter>
 
-</div>
+    </div>
 
 
-   
+
 }
-function Card({children}){
-    return <div style={{
-        background:"black", height:"300px",width:"300px", color:"black",padding:"10px",margin:"15px"
-    }}>{children}</div>
+function Class11Program(){
+    return <div>
+        Class11 Program For NEET
+    </div>
 }
+function Class12Program(){
+    return <div style={{color:"Black"}}>
+        Class12 Program For NEET
+    </div>
+}
+function Header(){
+    return <div>
+        Welcome to allen 
+        <Link to="/">Home |</Link> 
+ <Link to="/neet/online-coaching-class-11">Class 11 |</Link> 
+ <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    </div>
+}
+function Layout(){
+    return <div style={{backgroundColor:"red",}}>
+        <Header/>
+        <div style={{backgroundColor:"white" , height:"100vh",width:"100vw"}}>
+        <Outlet/>
+        </div>
+        footer
+    </div>
+}
+
+
+
+
+
 export default App
