7d267420cf866dada46c695c555801959f2734ea
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2024-12-21 13:21:53 +0530

    useprev hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState, } from 'react'
-import { useFetch } from './hooks/useFetch'
+import { usePrev } from './hooks/usePrev';
 import './App.css'
 
 
 function App() {
-  
-const {finalData} = useFetch("https://official-joke-api.appspot.com/random_joke");
- return <div>
- {JSON.stringify(finalData)}
- </div>
-}
+  const [count,setCount ] = useState(0);
+  const prevCount = usePrev(count);
 
+  return(
+    <>
+    <p>current count {count}</p>
+    <p>previous value was {prevCount}</p>
+    <button onClick={() => setCount(count + 1)}>Increase</button>
+
+
+    </>
+  )
+}
 
 
 export default App
